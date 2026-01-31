24105703fe8bbdbe86334a3c1aaff51bb8ad8c0a
VigneshM-96 <vignesh251974@gmail.com>
2026-01-31 13:40:29 +0530

    Second commit of Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      home: VoteChecker(),
-      debugShowCheckedModeBanner: false,
+      home : VoteChecker(),
+      debugShowCheckedModeBanner : false,
     );
   }
 }
 
-class VoteChecker extends StatefulWidget {
+class VoteChecker extends StatefulWidget{
   @override
   _VoteCheckerState createState() => _VoteCheckerState();
 }
@@ -23,54 +23,52 @@ class _VoteCheckerState extends State<VoteChecker> {
   TextEditingController ageController = TextEditingController();
   String result = "";
 
-  void checkEligibility() {
+  void checkEligibility(){
     int? age = int.tryParse(ageController.text);
 
     setState(() {
-      if (age == null) {
-        result = "Please enter a valid number";
-      } else if (age >= 18) {
-        result = "✅ Eligible to Vote";
-      } else {
-        result = "❌ Not Eligible to Vote";
+      if(age == null){
+        result = "Please enter valid number";
+      }else if(age >= 18){
+        result = "Eligibile to Vote";
+      }else{
+        result = "Not Eligible to Vote";
       }
     });
   }
-  
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar : AppBar (title : Text("Vote Eligibility Checker")),
-      body : Center (
+      appBar : AppBar(title : Text("Vote Eligibility Checker")),
+      body : Center(
         child : Padding(
-        padding : const EdgeInsets.all(20.0),
-        child : Column(
-          mainAxisAlignment : MainAxisAlignment.center,
-          crossAxisAlignment : CrossAxisAlignment.center,
-          children : [
-            TextField(
-              controller : ageController,
-              keyboardType : TextInputType.number,
-              decoration : InputDecoration(
-                labelText : "Enter your age",
-                border : OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height : 15),
-            ElevatedButton(
-              onPressed : checkEligibility,
-              child : Text("Check"),
-            ),
-
-            SizedBox(height : 20),
-            Text(
-              result,
-              style : TextStyle(fontSize : 18, fontWeight : FontWeight.bold),
-    
-            ),
-          ],
+          padding : const EdgeInsets.all(20.0),
+          child : Column(
+              mainAxisAlignment : MainAxisAlignment.center,
+              crossAxisAlignment : CrossAxisAlignment.center,
+              children : [
+                  TextField(
+                    controller : ageController,
+                    keyboardType : TextInputType.number,
+                    decoration : InputDecoration(
+                      labelText : "Enter your age",
+                      border : OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height : 15),
+                  ElevatedButton(
+                    onPressed : checkEligibility,
+                    child : Text("Check"),
+                  ),
+                  SizedBox(height : 70),
+                  Text(
+                    result,
+                    style : TextStyle(fontSize : 18, fontWeight : FontWeight.bold)
+                  ),
+              ],
+          ),
         ),
-      ),
       ),
     );
   }
